4290fc9e25fc113af1016d1be08b46d05dfe1894
Adalberto Taylor <adalbertotaylor84@gmail.com>
2021-01-29 00:07:27 -0300

    Adds book adapter

---
+++ src/book-search/book-adapter.ts
@@ -0,0 +1,55 @@
+import { Book } from '../book-list/BookList';
+
+interface BookListResponse {
+  items: BookResponse[],
+  totalItems: number
+}
+
+interface BookResponse {
+  id: string,
+  volumeInfo: BookVolumeResponse
+}
+
+interface BookVolumeResponse {
+  title: string,
+  authors: string[],
+  publisher: string,
+  publishedDate: string,
+  description: string,
+  imageLinks:  BookVolumeImageLinksResponse
+}
+
+interface BookVolumeImageLinksResponse {
+  thumbnail: string
+}
+
+const bookAdapter = (book: BookResponse): Book | undefined => {
+  if (
+      !book.id ||
+      !book.volumeInfo.imageLinks.thumbnail ||
+      !book.volumeInfo.title ||
+      !book.volumeInfo.authors ||
+      !book.volumeInfo.publisher ||
+      !book.volumeInfo.publishedDate ||
+      !book.volumeInfo.description
+  ) return
+
+  return {
+      id: book.id,
+      coverUrl: book.volumeInfo.imageLinks.thumbnail,
+      title: book.volumeInfo.title,
+      authors: book.volumeInfo.authors,
+      publisher: book.volumeInfo.publisher,
+      publishedDate: new Date(book.volumeInfo.publishedDate),
+      description: book.volumeInfo.description
+  }
+}
+
+function notUndefined<T>(x: T | undefined): x is T {
+  return x !== undefined;
+}
+
+export const bookListAdapter = (bookResponse: BookListResponse): Book[] => {
+  if(!bookResponse.totalItems) return []
+  return bookResponse.items.map(bookAdapter).filter(notUndefined)
+}
