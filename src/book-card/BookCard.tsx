a97431e66d433c431585c1b7c58166295fea56b7
Adalberto Taylor <adalbertotaylor84@gmail.com>
2021-02-01 12:05:56 -0300

    Adds add to wishlist button

--- src/book-card/BookCard.tsx
+++ src/book-card/BookCard.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { WishlistContext } from '../wishlist/useWishlist'
 import './BookCard.scss'
 
 export interface BookCardProps {
@@ -15,17 +16,26 @@ export interface Book {
   description: string,
 }
 
-export const BookCard: React.FC<BookCardProps> = ({ book }) => (
-  <article className="book-card">
-    <div className="cover">
-      <img src={book.coverUrl} alt={book.title} />
-    </div>
-    <div className="body">
-    <h2 className="title">
-      { book.title } by { book.authors[0] }
-    </h2>
-    <h4 className="subtitle">{ book.publisher } { book.publishedDate.toDateString() } </h4>
-    <p className="description">{ book.description }</p>
-    </div>
-  </article>
-)
+export const BookCard: React.FC<BookCardProps> = ({ book }) =>  {
+  const { addToWishlist } = WishlistContext.useContainer()
+
+  const addThisToWishlist = useCallback(() => {
+    addToWishlist(book)
+  }, [addToWishlist, book])
+
+  return (
+    <article className="book-card">
+      <div className="cover">
+        <img src={book.coverUrl} alt={book.title} />
+        <button onClick={addThisToWishlist}>Add to wishlist</button>
+      </div>
+      <div className="body">
+      <h2 className="title">
+        { book.title } by { book.authors[0] }
+      </h2>
+      <h4 className="subtitle">{ book.publisher } { book.publishedDate.toDateString() } </h4>
+      <p className="description">{ book.description }</p>
+      </div>
+    </article>
+  )
+}
